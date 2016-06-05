ac656503ad07295ce0ea7dfda780c4ed4550b078
saprette <samuel@prette.fr>
2016-06-05 17:28:15 +0000

    test with a lib

--- src/github.com/saprette/hello/hello.go
+++ src/github.com/saprette/hello/hello.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/saprette/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
-	fmt.Printf("Hello, github setup\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
 }
